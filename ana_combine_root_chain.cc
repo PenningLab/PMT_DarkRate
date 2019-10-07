6294f76fcfa27ddc00146158822a9eb8b5a4711c
luke <lime@bonniev2.0>
2019-10-07 14:31:06 -0400

    trig

--- ana_combine_root_chain.cc
+++ ana_combine_root_chain.cc
@@ -38,6 +38,7 @@
 #include <TH1F.h>
 #include <TH2F.h>
 #include <TH2Poly.h>
+#include <TH3F.h>
 #include <TKey.h>
 #include <TLine.h>
 #include <TMath.h>
@@ -69,8 +70,38 @@ static void show_usage(string name)
 	     << " -h or --help : Show the usage\n"
 	     << " Enjoy ! -Ryan Wang" << endl;
 }
+double SimpsIntegral(const float samples[], double baseline, int start, int end)
+{
+	int len;
+	double qsum = 0.0;
+	if ((end - start) % 2 == 0)
+	{
+		/* If there are an even number of samples, then there are an odd
+		number of intervals; but Simpson's rule works only on an even
+		number of intervals. Therefore we use Simpson's method on the
+		all but the final sample, and integrate the last interval
+		using the trapezoidal rule */
+		len = end - start - 1;
+		qsum += (samples[end - 1] + samples[end - 2] - 2 * baseline) / 2.0;
+	}
+	else
+		len = end - start;
+
+	double qsimps;
+	qsimps = samples[start] - baseline;
+	for (int i = start; i < start + len; i += 2)
+		qsimps += (samples[i] - baseline) * 4;
+	for (int i = start + 1; i < len + start - 1; i += 2)
+		qsimps += (samples[i] - baseline) * 2;
+	qsimps += samples[start + len - 1] - baseline;
+	qsimps /= 3.0;
+
+	qsum += qsimps;
+	return qsum;
+}
 int main(int argc, char* argv[])
 {
+
 	std::string filename;
 	std::string outfilename;
 	std::string working_dir;
@@ -91,6 +122,15 @@ int main(int argc, char* argv[])
 	std::string pydir;
 	int initial_run = 0;
 	float chargecut = 0;
+	float basecut = 1;
+	bool isS2 = false;
+	bool Qdep = false;
+	bool isSPE = false;
+	double resistance = 0.005;
+	double phd = 6.2;
+	bool watch = false;
+	int n;
+	float thresh;
 	if (argc < 2)
 	{
 		show_usage(argv[0]);
@@ -151,6 +191,32 @@ int main(int argc, char* argv[])
 		{
 			chargecut = atof(argv[i + 1]);
 		}
+		else if (arg == "-bcut")
+		{
+			basecut = atof(argv[i + 1]);
+		}
+		else if (arg == "-spe")
+		{
+			isSPE = true;
+		}
+		else if (arg == "-s2")
+		{
+			isS2 = true;
+		}
+		else if (arg == "-qdep")
+		{
+			Qdep = true;
+			n = atoi(argv[i + 1]);
+			thresh = atof(argv[i + 2]);
+		}
+		else if (arg == "-phd")
+		{
+			phd = atof(argv[i + 1]);
+		}
+		else if (arg == "-wa")
+		{
+			watch = true;
+		}
 	}
 	if (out_dir == "")
 		out_dir = working_dir;
@@ -191,6 +257,7 @@ int main(int argc, char* argv[])
 	float pulse_length1[kMaxPulses];
 	float pulse_length05[kMaxPulses];
 	float CalibratedTime[kMaxPulses];
+	float pulseFWHM[kMaxPulses];
 	// float found_pulses[kMaxPulses][kMaxPulseSamples];
 	// int found_pulses_nsamples[kMaxPulses];
 	// float windowratio;
@@ -210,6 +277,7 @@ int main(int argc, char* argv[])
 	float event_charge_ten;
 	float event_baseline;
 	float event_rms;
+	float event_basecharge;
 	float livetime;
 	float event_rate;
 	int npulses = 0;
@@ -227,7 +295,9 @@ int main(int argc, char* argv[])
 	event->Branch("fWindowCharge_pC", &event_windowcharge, "event_windowCharge/F");
 	event->Branch("fChargePrompt_pC", &event_charge_ten, "event_charge_ten/F");
 	event->Branch("fBaseline_V", &event_baseline, "event_baseline/F");
+	event->Branch("fBaselinerms_V", &event_rms, "event_rms/F");
 	event->Branch("bIsGood", &fillt, "fillt/O");
+	event->Branch("fBaseQ_rms", &event_basecharge, "event_basecharge/F");
 	event->Branch("nPulses", &npulses, "npulses/I");
 	event->Branch("fPulseHeight_V", amplitude, "amplitude[npulses]/F");
 	event->Branch("fPulseRightEdge", pr, "pr[npulses]/F");
@@ -235,6 +305,7 @@ int main(int argc, char* argv[])
 	event->Branch("fPulseCharge_pC", charge_v, "charge_v[npulses]/F");
 	event->Branch("fPulsePeakTime", amplitude_position, "amplitude_position[npulses]/F");
 	event->Branch("fCalibratedTime", CalibratedTime, "CalibratedTime[npulses]/F");
+	event->Branch("fFWHM", pulseFWHM, "pulseFWHM[npulses]/F");
 	event->Branch("fBigStep", biggeststep, "biggeststep[npulses]/F");
 	event->Branch("fPulseLength05", pulse_length05, "pulse_length05[npulses]/F");
 	event->Branch("fPulseLength1", pulse_length1, "pulse_length1[npulses]/F");
@@ -247,11 +318,11 @@ int main(int argc, char* argv[])
 	event->Branch("fPulseLength99", pulse_length99, "pulse_length99[npulses]/F");
 
 	TTree* filtertree = new TTree("filter", "filterTree");
-	float filterResponse, charge;
-	int n;
+	float filterResponse, charge, pfwhm;
 	filtertree->Branch("ADCCns", &filterResponse, "filterResponse/F");
 	filtertree->Branch("n", &n, "n/I");
 	filtertree->Branch("Charge", &charge, "charge/F");
+	// filtertree->Branch("FWHM", &pfwhm, "pfwhm/F");
 
 	if (use_trigger)
 	{
@@ -270,16 +341,49 @@ int main(int argc, char* argv[])
 	// int run=0;
 	vector<double> dark_count;
 	vector<double> dark_count_error;
+	int nmin = (isS2 ? 10 : 1);
+	int nmax = (isS2 ? 100 : 17);
+	int nbins = nmax - nmin + 2;
+	int maxth = 1000;
+	int minth = 0;
+	cout << "defining hists" << endl;
 	TH1F* h_avgphd = new TH1F("h_avgphd", "Average Pulse;Sample (10ns);ADC counts", 500, -250, 250);
 	h_avgphd->Sumw2();
-	TH2F* h_responsepop = new TH2F("h_responsepop",
-	    "Filter peak response density (SPE);n [samples];Peak Filter Response [ADCC*samples];Fraction of Pulses", 20, 0, 20, 2000, 0, 2000);
-	TH2F* h_responsepopno = new TH2F("h_responsepopno",
-	    "Filter peak response density (Noise);n [samples];Peak Filter Response [ADCC*samples];Fraction of Pulses", 20, 0, 20, 2000, 0, 2000);
-	TH2F* h_responseEff
-	    = new TH2F("h_responseEff", "Filter peak response efficiency;n [samples];Threshold [ADCC*samples];Efficiency", 20, 0, 20, 2000, 0, 2000);
-	TH2F* h_responseEffno
-	    = new TH2F("h_responseEffno", "Filter peak response efficiency;n [samples];Threshold [ADCC*samples];Efficiency", 20, 0, 20, 2000, 0, 2000);
+	cout << "defining pop hists" << endl;
+
+	TH2F* h_responsepop;
+	TH2F* h_responsepopno;
+	TH2F* h_responseEff;
+	TH2F* h_responseEffno;
+	TH2F* h_responseEffComb;
+	if (isSPE)
+	{
+		h_responsepop
+		    = new TH2F("h_responsepop", "Filter peak response density (SPE);n [samples];Peak Filter Response [ADCC*samples];Fraction of Pulses",
+		        nbins, nmin - 1, nmax + 1, maxth - minth, minth, maxth);
+		cout << "boo" << endl;
+		h_responsepopno
+		    = new TH2F("h_responsepopno", "Filter peak response density (Noise);n [samples];Peak Filter Response [ADCC*samples];Fraction of Pulses",
+		        nbins, nmin - 1, nmax + 1, maxth - minth, minth, maxth);
+		cout << "defining eff hists" << endl;
+		h_responseEff = new TH2F("h_responseEff", "Filter peak response efficiency;n [samples];Threshold [ADCC*samples];Efficiency", nbins, nmin - 1,
+		    nmax + 1, maxth - minth, minth, maxth);
+		h_responseEffno = new TH2F("h_responseEffno", "Filter peak response efficiency;n [samples];Threshold [ADCC*samples];Efficiency", nbins,
+		    nmin - 1, nmax + 1, maxth - minth, minth, maxth);
+		h_responseEffComb = new TH2F("h_responseEffComb", "Filter Combined efficiency;n [samples];Threshold [ADCC*samples];Efficiency", nbins,
+		    nmin - 1, nmax + 1, maxth - minth, minth, maxth);
+	}
+	TH3F* h_responsepopQ;
+	TH1F* h_QFrac;
+	TH1F* h_responseEffQ;
+	if (Qdep)
+	{
+		/*h_responsepopQ = new TH3F("h_responsepopQ", "Filter efficiency;n [samples];Threshold [ADCC*samples];Charge [phd];Efficiency", nbins, nmin -
+		   1, nmax + 1, 600, 0, 300, maxth - minth, minth, maxth);*/
+		h_QFrac = new TH1F("h_Qfrac", "Filter efficiency;Charge [phd];Efficiency", 3000, 0, 300);
+		h_responseEffQ = new TH1F("h_responseEffQ", "Filter efficiency;Charge [phd];Efficiency", 3000, 0, 300);
+	}
+
 	TH1D* h_sum = new TH1D("ADC_sum_waveform", ("#font[132]{WFD SumWaveForm}"), 10000, 0, 10000);
 	h_sum->SetXTitle("#font[132]{Sample (10ns)}");
 	h_sum->GetXaxis()->SetLabelFont(132);
@@ -314,7 +418,9 @@ int main(int argc, char* argv[])
 		tree->SetBranchAddress("fCharge_pC", &event_charge);
 		tree->SetBranchAddress("fChargePrompt_pC", &event_charge_ten);
 		tree->SetBranchAddress("fBaseline_V", &event_baseline);
+		tree->SetBranchAddress("fBaselinerms_V", &event_rms);
 		tree->SetBranchAddress("bIsGood", &fillt);
+		tree->SetBranchAddress("fBaseQ_rms", &event_basecharge);
 		tree->SetBranchAddress("fWindowCharge_pC", &event_windowcharge);
 
 		tree->SetBranchAddress("nPulses", &npulses);
@@ -324,6 +430,7 @@ int main(int argc, char* argv[])
 		tree->SetBranchAddress("fPulseCharge_pC", charge_v);
 		tree->SetBranchAddress("fPulsePeakTime", amplitude_position);
 		tree->SetBranchAddress("fCalibratedTime", CalibratedTime);
+		tree->SetBranchAddress("fFWHM", pulseFWHM);
 		tree->SetBranchAddress("fBigStep", biggeststep);
 		tree->SetBranchAddress("fPulseLength05", pulse_length05);
 		tree->SetBranchAddress("fPulseLength1", pulse_length1);
@@ -353,7 +460,14 @@ int main(int argc, char* argv[])
 		// tree->SetBranchAddress("triggerpulseWidth",&triggerpulseWidth);
 		// tree->SetBranchAddress("triggerpulsePeakTime",&triggerpulsePeakTime);
 		// cout << " processing root file No. " << i << endl;
+		float A = 1, B = 0.5;
+		if (isS2)
+		{
+			A = 0.5;
+			B = 1;
+		}
 		double numavp = 0;
+		int numQ = 0;
 		// flaot plotscale = 0;
 		int nument = tree->GetEntries();
 		for (int j = 0; j < nument; j++)
@@ -374,15 +488,15 @@ int main(int argc, char* argv[])
 				// waveforms[sam] = raw_waveform[sam] - thisbase;
 			}
 			int passedpulses = 0;
+			if (!fillt && (event_basecharge) > 0.1 * phd)
+			{
+				// fillt = false;
+				continue;
+			}
 			for (int k = 0; k < npulses; k++)
 			{
 				int initialsam = (use_trigger ? (pl[k] + triggerStartSam) : pl[k]);
 				int finalsam = (use_trigger ? (pr[k] + triggerStartSam) : pr[k]);
-				if (fillt)
-				{
-					// fillt = false;
-					break;
-				}
 				if (amplitude_position[k] > 1240 && amplitude_position[k] < 1150)
 					continue;
 				for (int ns = fmax(initialsam - 20, 0); ns < fmin(finalsam + 20, number_of_samples); ns++)
@@ -412,49 +526,162 @@ int main(int argc, char* argv[])
 				}
 				numavp++;
 				passedpulses++;
+				// pfwhm = pulseFWHM[k];
 
-				charge = charge_v[k];
-				if (charge > chargecut)
-					popscale += 1;
-				if (charge <= chargecut)
-					popscaleno += 1;
-				for (n = 1; n < 17; ++n)
+				numspes++;
+				if (Qdep && charge_v[k] > 0.25 * phd)
+				{
+					charge = charge_v[k] / phd;
+					// n = ;
+					float tempres;
+					filterResponse = 0;
+					int m = n;
+					if (isS2)
+						m = 4 * n;
+					// float descale = 1 / (8192);
+					float lastval = 0;
+					int ns = amplitude_position[k] + (n + m + n) / 2;
+					tempres = 0;
+					unsigned int endPoint = ns;
+					unsigned int startPoint = ((int)ns - n + 1 > 0 ? ns - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
+
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - m + 1 > 0 ? endPoint - m + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse += ((double)waveforms[i] * 8192) * A;
+
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - n + 1 > 0 ? endPoint - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
+
+					// filtertree->Fill();
+					h_QFrac->Fill(charge_v[k] / phd);
+					if (filterResponse > thresh)
+						h_responseEffQ->Fill(charge_v[k] / phd);
+					numQ++;
+				}
+			}
+			charge = 1000 * SimpsIntegral(waveforms, 0, 1180, 1250) / 5.0;
+			event_windowcharge = charge;
+
+			int inits = 1180;
+			int fins = 1300;
+
+			if (watch && (charge / phd) > chargecut)
+			{
+
+				// for (n = nmin; n <= nmax; ++n)
+				//{
+				n = 2;
+				char plotname[30];
+				sprintf(plotname, "n = %d", n);
+				TCanvas* c1 = new TCanvas(plotname);
+				TGraph* t11 = new TGraph();
+				TGraph* t22 = new TGraph();
+				float tempres;
+
+				int m = n;
+				if (isS2)
+					m = 4 * n;
+				// float descale = 1 / (8192);
+				float lastval = 0;
+				// int ns = winpeak + (n + m + n) / 2;
+				// tempres = 0;
+				for (int ns = inits; ns <= fins; ++ns)
+				{
+					filterResponse = 0;
+					unsigned int endPoint = ns;
+					unsigned int startPoint = ((int)ns - n + 1 > 0 ? ns - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
+
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - m + 1 > 0 ? endPoint - m + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse += ((double)waveforms[i] * 8192) * A;
+
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - n + 1 > 0 ? endPoint - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
+
+					t22->SetPoint(ns - inits, ns, filterResponse);
+				}
+				for (int ns = inits - 2 * n - m; ns <= fins; ++ns)
+				{
+					t11->SetPoint(ns - inits, ns, waveforms[ns] * 8192);
+				}
+				t11->SetMarkerStyle(2);
+				t22->SetMarkerColor(2);
+				t22->SetMarkerStyle(3);
+				t22->SetMarkerSize(3);
+
+				c1->cd(0);
+
+				t11->Draw("alp");
+
+				t22->Draw("p");
+
+				c1->Draw();
+				c1->Modified();
+				c1->Update();
+				c1->Print("stuf.png", "png");
+				std::cout << "new plot" << std::endl;
+				std::getchar();
+				//}
+			}
+			if (isSPE)
+			{
+				int winpeak = 0;
+				float winmax = 0;
+				for (int wp = 1180; wp < 1250; wp++)
+				{
+					if (waveforms[wp] > waveforms[winpeak])
+						winpeak = wp;
+				}
+				for (n = nmin; n <= nmax; ++n)
 				{
 					float tempres;
 					filterResponse = 0;
+					int m = n;
+					if (isS2)
+						m = 4 * n;
 					// float descale = 1 / (8192);
-					for (int ns = fmax(initialsam, 0); ns < fmin(finalsam, number_of_samples); ns++)
-					{
-						tempres = 0;
-						unsigned int endPoint = ns;
-						unsigned int startPoint = ((int)ns - n + 1 > 0 ? ns - n + 1 : 0);
-						for (unsigned int i = startPoint; i <= endPoint; ++i)
-							tempres -= ((double)waveforms[i] * 8192) * 0.5;
+					float lastval = 0;
+					int ns = winpeak + (n + m + n) / 2;
+					// tempres = 0;
+					unsigned int endPoint = ns;
+					unsigned int startPoint = ((int)ns - n + 1 > 0 ? ns - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
 
-						endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
-						startPoint = ((int)endPoint - n + 1 > 0 ? endPoint - n + 1 : 0);
-						for (unsigned int i = startPoint; i <= endPoint; ++i)
-							tempres += ((double)waveforms[i] * 8192) * 1;
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - m + 1 > 0 ? endPoint - m + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse += ((double)waveforms[i] * 8192) * A;
 
-						endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
-						startPoint = ((int)endPoint - n + 1 > 0 ? endPoint - n + 1 : 0);
-						for (unsigned int i = startPoint; i <= endPoint; ++i)
-							tempres -= ((double)waveforms[i] * 8192) * 0.5;
-						if (tempres > filterResponse)
-							filterResponse = tempres;
-					}
+					endPoint = (startPoint <= 1 ? 0 : startPoint - 1);
+					startPoint = ((int)endPoint - n + 1 > 0 ? endPoint - n + 1 : 0);
+					for (unsigned int i = startPoint; i <= endPoint; ++i)
+						filterResponse -= ((double)waveforms[i] * 8192) * B;
+
 					filtertree->Fill();
-					if (charge > chargecut)
+					if (charge > 0.25 * phd)
 					{
 						h_responsepop->Fill(n, filterResponse);
 					}
-
-					if (charge <= chargecut)
+					else
 					{
 						h_responsepopno->Fill(n, filterResponse);
 					}
 				}
-				numspes++;
+				if (charge > 0.25 * phd)
+					popscale += 1;
+				else
+					popscaleno += 1;
 			}
 			if (fillt)
 				npulses = passedpulses;
@@ -467,26 +694,83 @@ int main(int argc, char* argv[])
 		h_avgphd->Write();
 		event->Write();
 		filtertree->Write();
-		h_responsepop->Scale(1.0 / (double)popscale);
-		h_responsepopno->Scale(1.0 / (double)popscaleno);
-		for (int id = 0; id < h_responsepop->GetNbinsX(); id++)
+		if (isSPE)
 		{
-			float totEff = 0;
-			float totEffNo = 0;
-			int maxth = h_responsepop->GetNbinsY();
-			for (int ith = maxth; ith > 0; ith--)
+			for (int id = 0; id < h_responsepop->GetNbinsX(); id++)
 			{
-				totEff += h_responsepop->GetBinContent(id, ith);
-				h_responseEff->SetBinContent(id, ith, totEff);
-				totEffNo += h_responsepopno->GetBinContent(id, ith);
-				h_responseEffno->SetBinContent(id, ith, totEffNo);
+				float totEff = 0;
+				float totEffNo = 0;
+
+				int maxth = h_responsepop->GetNbinsY() + 1;
+				for (int ith = maxth; ith > 0; ith--)
+				{
+					totEff += h_responsepop->GetBinContent(id, ith) / (double)popscale;
+					totEffNo += h_responsepopno->GetBinContent(id, ith) / (double)popscaleno;
+					h_responseEff->SetBinContent(id, ith, totEff);
+					h_responseEffno->SetBinContent(id, ith, 1 - totEffNo);
+					h_responseEffComb->SetBinContent(id, ith, sqrt((1.0 - totEffNo) * totEff));
+				}
+			}
+			h_responsepop->Scale(1.0 / (double)popscale);
+			h_responsepopno->Scale(1.0 / (double)popscaleno);
+
+			h_responsepop->Write();
+			h_responseEff->Write();
+			h_responsepopno->Write();
+			h_responseEffno->Write();
+			h_responseEffComb->Write();
+
+			for (int i = nmin; i <= nmax; ++i)
+			{
+				int lobebin = h_responseEff->GetXaxis()->FindBin(i);
+				char canamae[80];
+				sprintf(canamae, "(n = %d)", i);
+				TCanvas* c1 = new TCanvas(canamae, canamae);
+				TGraph* g1 = new TGraph();
+				TGraph* g2 = new TGraph();
+				for (int j = 1; j <= maxth - minth; j++)
+				{
+					g1->SetPoint(j - 1, h_responseEff->GetYaxis()->GetBinCenter(j), h_responseEff->GetBinContent(lobebin, j));
+					g2->SetPoint(j - 1, h_responseEffno->GetYaxis()->GetBinCenter(j), h_responseEffno->GetBinContent(lobebin, j));
+				}
+				c1->cd();
+				g1->SetTitle("Efficiency");
+				g2->SetTitle("Noise Rejection Efficiency");
+				g1->SetMarkerColor(2);
+				g1->SetLineColor(2);
+				g2->SetMarkerColor(4);
+				g2->SetLineColor(4);
+				g1->GetYaxis()->SetRangeUser(0.996, 1);
+				g2->GetYaxis()->SetRangeUser(0.996, 1);
+				g1->Draw("alp");
+				g2->Draw("lp");
+				fout->cd();
+				c1->Write();
 			}
 		}
-		std::cout << numspes << " SPEs in tree" << std::endl;
-		h_responsepop->Write();
-		h_responseEff->Write();
-		h_responsepopno->Write();
-		h_responseEffno->Write();
+		if (Qdep)
+		{
+			/*
+			for (int id = 0; id < h_responsepopQ->GetNbinsX(); id++)
+			{
+
+			    for (int iq = 0; iq < h_responsepopQ->GetNbinsY(); iq++)
+			    {
+			        float totEffQ = 0;
+			        int maxth = h_responsepopQ->GetNbinsZ() + 1;
+			        for (int ith = maxth; ith > 0; ith--)
+			        {
+			            totEffQ += h_responsepopQ->GetBinContent(id, iq, ith) / (double)numQ;
+			            h_responseEffQ->SetBinContent(id, iq, ith);
+			        }
+			    }
+			}
+			h_responsepopQ->Scale(1.0 / (double)numQ);
+			h_responseEffQ->Write();*/
+			h_responseEffQ->Divide(h_QFrac);
+			h_responseEffQ->Write();
+		}
+		std::cout << popscale << " SPEs in tree " << numQ << std::endl;
 	}
 
 	fout->Write();
